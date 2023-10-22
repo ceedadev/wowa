274cc435f888e3f5fadb80558dbb48320ebecd42
ceedadev <ceedadev@gmail.com>
2023-10-22 11:34:30 +0700

    fix paddings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import PhoneNumberInput from "@/components/phone-input";
 export default function Home() {
   return (
     <div className="mb-auto">
-      <div className="m-32">
+      <div className="mt-32 mx-16">
         <h1 className="text-center mb-24 font-semibold tracking-tight">
           Send Whatsapp Messages, without saving the number to contact.
         </h1>
